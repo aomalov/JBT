98132e7ba5aebc6515c5c68db7d84f6fc1bc90db
unknown <andrewm@pc-il-andrewm.Traiana.INT>
2015-12-13 16:05:15 +0200

    Added some security to the project: passwords stored in DB - hashed and salted

---
+++ src/com/jbt/jsmith/test/TestSecureLogin.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.jbt.jsmith.test;
+
+import java.sql.Connection;
+
+import com.jbt.jsmith.CouponSystemException;
+import com.jbt.jsmith.dao.ConnectionPool;
+import com.jbt.jsmith.dao.security.Owasp;
+
+/**
+ * @author andrewm
+ *
+ */
+public class TestSecureLogin {
+
+	/**
+	 * @param args
+	 * @throws CouponSystemException 
+	 */
+	public static void main(String[] args) throws CouponSystemException {
+		// TODO Auto-generated method stub
+		
+		ConnectionPool cPool=ConnectionPool.getInstance(ConnectionPool.defDriverName, ConnectionPool.defDbUrl);
+		Connection conn=cPool.getConnection();
+		
+		try {
+			Owasp.createTable(conn);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		try {
+			Owasp.createUser(conn, "company1", "pass_Company1");
+			System.out.println("Authenticated:"+Owasp.authenticate(conn, "company1", "pass_Company1"));
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
